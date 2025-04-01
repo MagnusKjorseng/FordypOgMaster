1c9be7fd3e773a4fa9c6c6cb499d23573d25e086
MagnusKjorseng <143486202+MagnusKjorseng@users.noreply.github.com>
2025-04-01 10:09:28 +0200

    Update rov_controller.py

--- Simulator/V4/build/controller/build/lib/controller/rov_controller.py
+++ Simulator/V4/build/controller/build/lib/controller/rov_controller.py
@@ -8,42 +8,49 @@ import numpy as np
 def main(args = None):
     rclpy.init(args=args)
 
-    controller = CraneController()
+    controller = RovController()
     rclpy.spin(controller)
 
-class CraneController(Node):
-    def __init__(self):
-        super().__init__("crane_controller")
+class RovController(Node):
+    def __init__(self, target_depth):
+        super().__init__("rov_controller")
 
-        self.target_depth = 0
+        self.target_depth = target_depth
+        self.position = None
+        self.rotation = None
 
-        self.rov_listener = self.create_subscription(geo_msgs.Pose,
+        self.pose_listener = self.create_subscription(geo_msgs.Pose,
                                                 "rov_pose",
                                                 self.pose_callback,
                                                 5)
-        self.desired_depth_listener = self.create_subscription(std_msgs.Float32,
-                                                               "desired_rov_depth",
-                                                               self.update_depth_callback,
+        self.vel_listener = self.create_subscription(geo_msgs.Vector3,
+                                                     "rov_velocity",
+                                                     self.vel_callback,
+                                                     5)
+        self.desired_position_listener = self.create_subscription(geo_msgs.Pose,
+                                                               "desired_rov_pose",
+                                                               self.update_pose_callback,
                                                                5)
         self.rov_force_publisher = self.create_publisher(geo_msgs.Vector3,
-                                                         "rov_force",
+                                                         "rov_desired_force_on_cog",
                                                          5)
 
 
     def pose_callback(self, msg):
-        position = msg.position
+        self.position = msg.position
+        self.rotation = msg.orientation
 
 
-    def control_depth(self, current_depth, target_depth):
+    def control_position(self, current, target):
         kp, ki, kd = 5,0,0
 
-        error = target_depth - current_depth
+        error = target - current
 
         controlled = kp * error
 
         return controlled
 
-    def update_depth_callback(self, msg):
+    def update_pose_callback(self, msg):
         desired_depth = msg.data
 
         self.target_depth = desired_depth
