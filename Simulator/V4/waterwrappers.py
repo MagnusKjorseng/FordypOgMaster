0683eb71b505c6c311dcadf4040a55aa4731f352
MagnusKjorseng <143486202+MagnusKjorseng@users.noreply.github.com>
2025-04-01 15:18:31 +0200

    Implemented a better wave simulation

--- Simulator/V4/waterwrappers.py
+++ Simulator/V4/waterwrappers.py
@@ -3,17 +3,11 @@ import math
 
 class WaveWaterWrapper(agxModel.WaterWrapper):
     #Shamelessly stolen from the hydrodynamics tutorial for AGX
-    def __init__(self):
+    def __init__(self, wave_heigh):
         super().__init__()
+        self.wave_heigh = wave_height
 
     def findHeightFromSurface(self, worldPoint, upVector, t):
-        surface_level = -1 + 0.5 * math.sin(0.5 * worldPoint.x() + 0.6 * t) + \
-            0.25 * math.cos(0.6 * worldPoint.y() + 0.3 * worldPoint.x() + 1.45 * t)
+        surface_level = self.wave_height*(0.5 * math.sin(0.5 * worldPoint.x() + 0.6 * t) + \
+            0.25 * math.cos(0.6 * worldPoint.y() + 0.3 * worldPoint.x() + 1.45 * t))
         return worldPoint.z() - surface_level
-
-class StillWaterWrapper(agxModel.WaterWrapper):
-    def __init__(self):
-        super().__init__()
-        
-    def findHeightFromSurface(self, worldPoint, upVector, t):
-        return worldPoint.z()
