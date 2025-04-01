c2fadee08bc87b86aa91205a01f552f460589947
MagnusKjorseng <143486202+MagnusKjorseng@users.noreply.github.com>
2025-04-01 10:08:22 +0200

    DAta collector working and started. Still needs more plot points

--- Simulator/V4/controller/controller/data_collector.py
+++ Simulator/V4/controller/controller/data_collector.py
@@ -1,9 +1,10 @@
 import rclpy
 from rclpy.node import Node
 import std_msgs.msg as std_msgs
-import geo_msgs.msg as geo_msgs
+import geometry_msgs.msg as geo_msgs
 
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 import matplotlib.pyplot as plt
 
@@ -17,20 +18,106 @@ class DataCollector(Node):
     def __init__(self):
         super().__init__("data_collector")
 
+        self.usv_target = [0.,0.,0.]
+        self.rov_target = [0.,0.,-50.]
+
+        self.plot_timer = self.create_timer(5, self.plot)
+
+        self.usv_poses = []
         self.usv_pose_listener = self.create_subscription(geo_msgs.Pose,
                                                       "usv_pose",
-                                                      self.position_callback,
+                                                      self.usv_position_callback,
                                                       5)
 
-
+        self.rov_poses = []
         self.rov_pose_listener = self.create_subscription(geo_msgs.Pose,
                                                       "rov_pose",
-                                                      self.position_callback,
+                                                      self.rov_position_callback,
                                                       5)
+
+        self.usv_forces = []
+        self.usv_force_listener = self.create_subscription(geo_msgs.Wrench,
+                                                           "usv_desired_force_on_cog",
+                                                           self.usv_force_callback,
+                                                           5)
+        self.rov_forces = []
+        self.rov_force_listener = self.create_subscription(geo_msgs.Wrench,
+                                                           "rov_desired_force_on_cog",
+                                                           self.rov_force_callback,
+                                                           5)
+        self.crane_responses = []
+        self.crane_listener  = self.create_subscription(std_msgs.Float32,
+                                                        "winch_speed",
+                                                        self.crane_callback,
+                                                        5)
+
+
+    def plot(self):
+        usv_poses = np.array(self.usv_poses)
+        usv_positions = usv_poses[:, 0]
+        usv_orientations = usv_poses[:,1]
+
+        usv_position_errors = self.usv_target - usv_positions
+        usv_position_mse = np.square(usv_position_errors).mean(axis=1)
+        print(usv_positions)
+        plt.plot(usv_positions[:,0])
+
+        plt.show()
+
+        rov_poses = np.array(self.rov_poses)
+        rov_positions = rov_poses[0,:]
+        rov_orientations = rov_poses[1,:]
+
+        usv_forces = np.array(self.usv_forces)
+        usv_force = usv_forces[0,:]
+        usv_torque = usv_forces[1,:]
+
+        rov_forces = np.array(self.rov_forces)
+        rov_force = rov_forces[0,:]
+        rov_torque = rov_forces[1,:]
+
+
+
+
 
     def position_callback(self, msg):
         position = msg.position
         position = [position.x, position.y, position.z]
+        orientation = msg.orientation
+        orientation = [orientation.x, orientation.y, orientation.z, orientation.w]
+        r = R.from_quat(orientation)
+        orientation = r.as_euler("xyz", degrees = True)
+
+        return position, orientation
+
+    def usv_position_callback(self,msg):
+        position, orientation = self.position_callback(msg)
+        self.usv_poses.append([position, orientation])
+
+    def rov_position_callback(self, msg):
+        position, orientation = self.position_callback(msg)
+        self.rov_poses.append([position, orientation])
+
+    def force_callback(self, msg):
+        force = msg.force
+        torque = msg.torque
+        force = [force.x, force.y, force.z]
+        torque = [torque.x, torque.y, torque.z]
+
+        return force, torque
+
+    def usv_force_callback(self, msg):
+        force, torque = self.force_callback(msg)
+        usv_forces.append([force, torque])
+
+    def rov_force_callback(self, msg):
+        force, torque = self.force_callback(msg)
+        rov_forces.append([force, torque])
+
+    def crane_callback(self, msg):
+        return
+
+
 
 if __name__ == '__main__':
     main()
